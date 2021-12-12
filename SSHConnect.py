5469bb9795c3b48bf6dd712a4e83c357022131a9
yasu-code <uema.sa.m4@gmail.com>
2021-12-12 15:16:45 +0000

    add new file

---
+++ SSHConnect.py
@@ -0,0 +1,34 @@
+import json
+import paramiko
+
+
+class SSHConnect():
+    def __init__(self):
+        self.json_open = open('connection_address.json', 'r')
+        self.json_load = json.load(self.json_open)
+        self.connection_info = []
+
+        for v in self.json_load:
+            self.connection_info.append(v)
+
+
+    def connection(self, ip, port, user):
+        self.client = paramiko.SSHClient()
+        self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.client.connect(ip, port=port, username=user)
+        self.sftp_connection = self.client.open_sftp()
+
+    def put(self, local_file, remote_file):
+        try:
+            for info in self.connection_info:
+                self.connection(info["ip"], info["port"], info["user"])
+                self.sftp_connection.put(local_file, remote_file)
+                print(info["ip"] + " completed!!")
+                self.client.close()
+        except(e):
+            print(e)
+        finally:
+            self.client.close()
+        
+ssh = SSHConnect()
+ssh.put("text.txt", "/tmp/text.txt")
